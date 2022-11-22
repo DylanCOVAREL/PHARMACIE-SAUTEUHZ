9ca4b977d65d680a3624768e8e24d3662a8cb269
Ka-2000 <kamil.zidi@outlook.fr>
2022-11-22 17:09:22 +0100

    Ajout du IndexJS + eror404 + modification des fichier pour etre mieux organiser

---
+++ index.js
@@ -0,0 +1,28 @@
+// inclure les dépendances et middlewares
+const mysql = require('mysql')
+const express = require('express')
+const ejs = require('ejs')
+const session = require('express-session');
+const flash = require('connect-flash');
+const iniparser = require('iniparser')
+const Routeur = require('./routes/routes');
+
+// activer les dépendances
+let app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('param'))
+app.use(express.static('views'))
+app.use(express.urlencoded());
+app.use(session({
+	secret:'HelloWorld!',
+	saveUninitialized: true,
+	resave: true
+}));
+app.use(flash());
+
+app.listen(3000, () => console.log('Le serveur est fonctionnel !'))
+app.get('/', (req, res) => {
+    res.send('Le serveur est fonctionnel !')
+})
+
+app.use('/PHARMACIE-SAUTEUHZ', Routeur)
